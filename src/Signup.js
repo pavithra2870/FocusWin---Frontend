a007322a235bf075dbda7add2080edd305a83294
Pavithra K R <pavithra922710@gmail.com>
2025-07-08 14:48:39 +0530

    Update Signup.js

--- src/Signup.js
+++ src/Signup.js
@@ -16,7 +16,7 @@ function Signup() {
     e.preventDefault();
     setError('');
     setLoading(true);
-    const res = await fetch('http://localhost:5000/api/auth/signup', {
+    const res = await fetch('https://focuswin.onrender.com/api/auth/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       credentials: 'include',
