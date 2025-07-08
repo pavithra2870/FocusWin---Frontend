d65083b86bafefc1a5945de91ab05da5c66e6596
Pavithra K R <pavithra922710@gmail.com>
2025-07-08 16:34:20 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -15,7 +15,7 @@ function Login() {
     e.preventDefault();
     setError('');
     setLoading(true);
-    const res = await fetch('http://localhost:5000/api/auth/login', {
+    const res = await fetch('https://focuswin.onrender.com/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       credentials: 'include',
